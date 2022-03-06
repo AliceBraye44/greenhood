2390053597babdc44a90c6470568cb094b24d3ef
Alice Braye <braye.alice@gmail.com>
2022-03-06 20:13:09 +0100

    service creation transform adress to GPS coordinates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\CallApiService;
+use App\Service\TransformAdressGeo;
+
 
 class HomeController extends AbstractController
 {
@@ -13,9 +15,9 @@ class HomeController extends AbstractController
     
     
     #[Route('/home', name: 'app_home')]
-    public function index(CallApiService $callApi): Response
+    public function index(CallApiService $callApi, TransformAdressGeo $transformAdress): Response
     {
-        dd($callApi->getData());
+        dd($transformAdress->geocodeAddress());
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
