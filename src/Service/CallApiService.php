6c4263d0bb90e400a36ab653fc6db90653b87e1a
Alice Braye <braye.alice@gmail.com>
2022-03-06 19:20:28 +0100

    add exemple decheterie

--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -14,11 +14,12 @@ class CallApiService
         $this->client = $client;
     }
 
-    public function getData($dataset): array
+    public function getData(): array
     {
         $response = $this->client->request(
             'GET',
-            'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset='.$dataset
+            //'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset='.$dataset
+            'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_decheteries-ecopoints-nantes-metropole'
         );
         return $response->toArray(); 
     }
