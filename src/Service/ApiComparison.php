7a361db1ac943d94e2539810761180123ad97937
Alice Braye <braye.alice@gmail.com>
2022-03-28 17:28:30 +0200

    création d'une variante de l'algo avec non plus la définition d'un perimetre autour d'un point donnée et la vérification de la présence d'item ou non dans la zone mais un calcul de distance entre chaque item et le point donné. si cette dernière est inférieure ou égale au perimetre stocké en bdd, alors le point est inclus dans la zone

--- src/Service/ApiComparison.php
+++ src/Service/ApiComparison.php
@@ -3,9 +3,10 @@
 namespace App\Service;
 use App\Service\PerimeterCalculation;
 use App\Service\CallApiService;
+use App\Service\TransformAdressGeo;
 use App\Repository\CriteriaRepository;
 use Location\Coordinate;
-use Location\Polygon;
+use Location\Distance\Vincenty;
 
 
 
@@ -14,7 +15,7 @@ class ApiComparison
 
     function letSCalculate(
         CriteriaRepository $criteriaRepository,
-        PerimeterCaluculation $perimeterCalculation,
+        TransformAdressGeo $adress,
         CallApiService $callApi,
         $initialAdress
         ) {
@@ -28,16 +29,15 @@ class ApiComparison
             //Creation d'un tableau de réponses positives
             $matches = [];
 
-            //Définition du périmetre de comparaison
-            $areaToCompare = $perimeterCalculation->getArea($initialAdress, $criteria->getId());
+            // TODO Récupération de l’adresse par methode POST
+            $initialAdress; 
+    
+            // Transformation de l’adresse en coordonnées GPS 
+            $coordinatesToCompare = $adress->geocodeAddress($initialAdress);
            
-            //Définition du perimetre à comparer 
-            $polygonToCompare = new Polygon();
+            // Transformation des coordonnées GPS en cordonnées exploitatable par phpgeo
+            $coordinatesToCompare = new Coordinate($coordinatesToCompare[0], $coordinatesToCompare[1]); 
 
-            foreach ($areaToCompare as $coordinate) {
-                $polygonToCompare->addPoint(new Coordinate($coordinate));
-            }
-        
             //Appel de l'api 
             $results = $callApi->getDataApi($criteria->getData());
 
@@ -49,11 +49,17 @@ class ApiComparison
                 
                 // Stockage des coordonnées des items de l'Api à fin de comparaison
                 $itemCoordinates = new Coordinate($coordX, $coordY);
-                
+
+                // Comparaison des deux coordonnées pour en obtenir la distance grâce à la formule Vincenty (phpgeo)
+                $calculator = new Vincenty();
+                $distance = $calculator->getDistance($coordinatesToCompare, $itemCoordinates);
+                //TODO vérifier les mesures du resultat 
+        
                 // si l'item est inclus dans le perimetre de recherche, ajout de l'items à la liste des résultats positifs 
-                if( $polygonToCompare->contains($itemCoordinates) ){
+                if( $distance < $criteria->getPerimeter() ) {
                     array_push($matches, [$cordX, $coordY]);
                 }
+                
             }
 
             // Décompte des résultats positifs
@@ -63,7 +69,7 @@ class ApiComparison
             $score = $criteria->getIndexReference()/$numberOccurences;
 
             //retourne un tableau par critère incluant : le nom du critère, le score, le tableau de matchs avec les coordonnées, le pin associé
-            $returnByCriteria = [$criteria->getName(), $score,  $matches, $criteria->getPin()];
+            $returnByCriteria = [" name" => $criteria->getName(), "score" => $score,  "itemsCoord" => $matches, "pin" => $criteria->getPin()];
             
             return $returnByCriteria;
                
